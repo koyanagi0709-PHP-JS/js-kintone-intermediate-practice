1861c1d2f144dd2c0e87a74d81e6493a6bce72f3
koyanagi0709 <koyanagi0709@gmail.com>
2026-06-10 05:25:58 +0900

    変数の使い方について

---
+++ 01_Start_JavaScript/03_start_javascript.js
@@ -0,0 +1,13 @@
+console.log('Hello, world!');
+console.log('Hello, world!');
+console.log('Hello, world!');
+console.log('Hello, world!');
+console.log('Hello, world!');
+
+const hello = 'Hello, world!';
+
+console.log(hello);
+console.log(hello);
+console.log(hello);
+console.log(hello);
+console.log(hello);
